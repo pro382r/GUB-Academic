2d274c8be08b36dcf0d96244c4f0f2cac638e75d
💜 Rihun 💊 <pro382gamer@gmail.com>
2026-06-22 02:31:54 +0600

    Add files via upload

---
+++ AI-lab/lr1/7-python_20260621_16155b.py
@@ -0,0 +1,14 @@
+# 7. Swap Two Tuples
+
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+
+print("Before Swapping:")
+print("Tuple 1 =", t1)
+print("Tuple 2 =", t2)
+
+t1, t2 = t2, t1
+
+print("\nAfter Swapping:")
+print("Tuple 1 =", t1)
+print("Tuple 2 =", t2)
